61fbe7a88f8a5330d96322c452ea857bb89c108d
edsonbastos2 <edsonbastos2@hotmail.com>
2017-06-14 17:07:12 -0300

    Nova atualização
    atualização da classe Sql

--- class/Sql.php
+++ class/Sql.php
@@ -4,46 +4,47 @@
 
 		private $conn;
 
+
 		public function __construct(){
+
 
 			$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 		}
 
-		private function setParams($statment, $parameters = array()){
+
+		private function setParams($statement, $parameters = array()){
 
 			foreach ($parameters as $key => $value) {
 				
-				$this->setParam($key, $value);
-
+				$this->setParam($statement, $key, $value);
+			}
 		}
 
-	}
 
+		private function setParam($statement, $key, $value){
 
-		private function setParam($statment, $key, $value){
-
-			$statment->bindParam($key, $value);
+			$statement->bindParam($key, $value);
 		}
+
 
 		public function query($rawQuery, $params = array()){
 
-			$stmt = $this->conn->prepare($rawQuery);
+				$stmt = $this->conn->prepare($rawQuery);
 
-			$this->setParams($stmt, $params);
+				$this->setParams($stmt, $params);
 
-			$stmt->execute();
+				$stmt->execute();
 
-			return $stmt;
-			
+				return $stmt;
 		}
 
-		public function select($rawQuery, $params = array()):array
+		public function select($rawQuery, $params = array())
 		{
 
 			$stmt = $this->query($rawQuery, $params);
 
 			return $stmt->fetchall(PDO::FETCH_ASSOC);
 		}
-		
 	}
+
  ?>
